ea3e281139827f779b03b350f9d376c22c4424b3
Mark Bense <marksbense@gmail.com>
2020-04-05 21:58:26 -0400

    disable logging

--- log.cpp
+++ log.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <thread>
+#include <fstream>
+#include <mutex>
 
 
 void print(std::string msg) {
     std::stringstream sstr;
     sstr << msg << std::endl;
-    // std::cerr << sstr.str();
+    std::cerr << sstr.str();
 }
 
 void thread_print(std::string msg) {
-    std::stringstream sstr;
-    sstr << "Thread " << std::this_thread::get_id() << ": " << msg << std::endl;
-    // std::cerr << sstr.str();
+    // static std::mutex m;
+    // std::lock_guard<std::mutex> lock(m);
+    // std::stringstream sstr;
+    // sstr << "Thread " << std::this_thread::get_id() << ": " << msg << std::endl;
+    // // std::cerr << sstr.str();
+    // std::ofstream out;
+    // static bool append = false;
+    // if (!append)
+    //     out.open("applog.txt", std::ios_base::app);
+    // else {
+    //     out.open("applog.txt");
+    //     append = true;
+    // }
+    // out << sstr.str();
+    // out.close();
+
 }
